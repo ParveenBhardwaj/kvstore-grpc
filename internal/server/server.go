9deb1b45dc90af48a2d71fb5d8739a171c6f28b6
ParveenBhardwaj <parveen-bhardwaj@outlook.com>
2025-07-05 22:26:47 +1000

    Add get and delete key value

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,46 @@
 package server
+
+import (
+	"context"
+	"fmt"
+
+	"kvstore-grpc/gen/kvstorepb"
+)
+
+type InMemoryKVStore struct {
+	kvstorepb.UnimplementedKVStoreServer // Required for forward compatibility
+	store                                map[string]string
+}
+
+func NewInMemoryKVStore() *InMemoryKVStore {
+	return &InMemoryKVStore{
+		store: make(map[string]string),
+	}
+}
+
+func (s *InMemoryKVStore) Set(ctx context.Context, req *kvstorepb.SetRequest) (*kvstorepb.Empty, error) {
+	s.store[req.Key] = req.Value
+	return &kvstorepb.Empty{}, nil
+}
+
+func (s *InMemoryKVStore) Get(ctx context.Context, req *kvstorepb.GetRequest) (*kvstorepb.GetResponse, error) {
+	// Get key from store
+	value, found := s.store[req.Key]
+	// Check if key exists in store
+	if !found {
+		return nil, fmt.Errorf("key %v not found", req.Key)
+	}
+	return &kvstorepb.GetResponse{
+		Value: value,
+	}, nil
+}
+
+func (s *InMemoryKVStore) Delete(ctx context.Context, req *kvstorepb.DeleteRequest) (*kvstorepb.Empty, error) {
+	// Check if key exists in store
+	_, found := s.store[req.Key]
+	if !found {
+		return nil, fmt.Errorf("key %v not found", req.Key)
+	}
+	delete(s.store, req.Key)
+	return &kvstorepb.Empty{}, nil
+}
--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -8,6 +8,7 @@ import (
 )
 
 func TestSetStoreValue(t *testing.T) {
+	// Create a new store
 	s := NewInMemoryKVStore()
 
 	req := &kvstorepb.SetRequest{
@@ -27,4 +28,60 @@ func TestSetStoreValue(t *testing.T) {
 	if got != want {
 		t.Errorf("expected value %q, got %q", want, got)
 	}
+}
+
+func TestGetStoreValue(t *testing.T) {
+	//  Create a new store
+	s := NewInMemoryKVStore()
+	req := &kvstorepb.SetRequest{
+		Key:   "hello",
+		Value: "world",
+	}
+
+	_, err := s.Set(context.Background(), req)
+	if err != nil {
+		t.Fatalf("Set failed: %v", err)
+	}
+
+	getReq := &kvstorepb.GetRequest{
+		Key: "hello",
+	}
+
+	got, err := s.Get(context.Background(), getReq)
+	if err != nil {
+		t.Fatalf("Get failed: %v", err)
+	}
+	want := "world"
+
+	if got.Value != want {
+		t.Errorf("expected value %q, got %q", want, got)
+	}
+
+}
+
+func TestDeleteStoreValue(t *testing.T) {
+	// Create a new store
+	s := NewInMemoryKVStore()
+	setReq := &kvstorepb.SetRequest{
+		Key:   "hello",
+		Value: "world",
+	}
+
+	_, err := s.Set(context.Background(), setReq)
+	if err != nil {
+		t.Fatalf("Set failed: %v", err)
+	}
+
+	deleteReq := &kvstorepb.DeleteRequest{
+		Key: "hello",
+	}
+	_, err = s.Delete(context.Background(), deleteReq)
+	if err != nil {
+		t.Fatalf("Delete failed: %v", err)
+	}
+
+	if len(s.store) != 0 {
+		t.Fatalf("Expected store to be empty, but store lenght is %v", len(s.store))
+	}
+
 }
